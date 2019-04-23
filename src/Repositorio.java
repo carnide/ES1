e01dc5b74aa44d28b536fc6bb3b32507a24323e9
carnide <pedro.m.carnide@gmail.com>
2019-04-23 20:41:21 +0100

    Primeiro commit

--- src/Repositorio.java
+++ src/Repositorio.java
@@ -1,5 +1,18 @@
 public interface Repositorio { /*porque o repositorio não liga a nada*/
     /*ou public class abstarct*/
 
-
+     void adicionaUtilizador(Utilizador utilizador);
+     void adicionaTipoUtilizador(TipoUtilizador tipo);
+     void adicionaRequisicao(Requisicao requisicao);
+     void adicionaCopia(Copia copia);
+     void adicionaLivro(Livro livro);
+     void adicionaEmprestimo(Emprestimo emprestimo);
+     void adicionaNotificacao(Notificacao notificacao);
+     void adicionaDevolucao(Devolucao devolucao);
+     void adicionaCoima(Coima coima);
+     Emprestimo devolveEmprestimoDaRequisicao(Requisicao r);
+     void	adicionaEncomenda(Encomenda encomenda);
+     void	adicionaEntradaNovoLivro(EntradaNovoLivro entradaNovoLivro);
+     void	adicionaPropostaRequisicao(PropostaAquisicao proposta);
+     void	adicionaRequisicaoCompra(RequisicaoCompra requisicaoCompra);
 }
